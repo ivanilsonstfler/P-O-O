dcb54e78892fea6bfd5683a44fe44272fb32bee3
ivailson <ivanilson57637@estudante.ifb.edu.br>
2024-07-10 20:02:57 -0300

    njkbh

---
+++ gustavoguanabara/bosnowhile.py
@@ -0,0 +1,12 @@
+# nome = None
+
+# while True:
+#     print('Digite seu nome, ou X para parar:')
+
+#     nome = input()
+#     if nome == 'x' or nome == 'X':
+#         break
+
+#     print(f'Bem-Vindo, {nome}')
+
+# print("Até logo!")
